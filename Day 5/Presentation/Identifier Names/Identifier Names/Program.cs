897e4d0a74dad313746bbc96ac5a10ef6c137189
afiqster12 <afiqwork12@outlook.com>
2022-01-20 21:05:28 +0800

    Presentation Codes

---
+++ Day 5/Presentation/Identifier Names/Identifier Names/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Identifier_Names
+{
+    // Interface - > IThenFirstLetterCapital
+    interface IEmployee 
+    {
+        void PrintEmployeeDetails();
+    }
+    // Class Name -> CapitalFirstLetter -> Nouns
+    class Employee : IEmployee 
+    {
+        // Private Variable Name -> nonCapitalFirstLetter -> Nouns
+        private int employeeID;
+        // Public Variable Name -> CapitalFirstLetter -> Nouns
+        public string Name;
+        public Employee(int employeeID, string name)
+        {
+            this.employeeID = employeeID;
+            Name = name;
+        }
+        // Method Name -> CapitalFirstLetter -> Verbs e.g. Print,Check
+        public void PrintEmployeeDetails()
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("Employee ID: " + employeeID);
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("--------------------------");
+        }
+    }
+    class Program
+    {
+        //Readonly Variable -> _nonCapitalFirstLetter -> Nouns
+        readonly string _readOnlyVariable = "This is a readonly variable";
+        //Constant Variable -> ALLCAPITALLETTERS -> Nouns
+        const double PI = 3.14159;
+        static void Main(string[] args)
+        {
+            IEmployee employee = new Employee(1, "John");
+            employee.PrintEmployeeDetails();
+        }
+    }
+}
